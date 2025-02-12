a73d978c8347296435b34163ba54665fdb60f9c6
MateoHaxhi07 <mhaxhi62@gmail.com>
2025-02-12 21:04:30 +0100

    It

--- run_backend_server.py
+++ run_backend_server.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 
 # Change directory to the restaurant-backend folder
-os.chdir(r'C:\Users\mhaxh\OneDrive\Desktop\Restaurant_Dashboard-1.0.8\restaurant-backend')
+os.chdir(r'C:\Users\mhaxh\OneDrive\Desktop\Restaurant_Dashboard-1.2.0\restaurant-backend')
 
 # Run the 'node server.js' command to start the backend server
 subprocess.run(['node', 'server.js'], shell=True)
--- run_npm_start.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import os
-
-# Change directory to the specified path
-os.chdir(r'C:\Users\mhaxh\OneDrive\Desktop\Restaurant_Dashboard-1.0.8')
-
-# Run the 'npm start' command in the terminal
-subprocess.run(['npm', 'start'], shell=True)
