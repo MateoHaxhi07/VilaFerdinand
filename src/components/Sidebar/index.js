293f807f3e3daf6d153ef3e356bd6cb965408bed
MateoHaxhi07 <mhaxhi62@gmail.com>
2025-02-13 20:30:25 +0100

    t

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
   Box,
   Button,
@@ -13,42 +14,47 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 const SidebarContent = ({ onClick }) => (
-  <VStack>
+  <VStack spacing={4} align="stretch">
     <Button as={Link} to="/home" w="100%" onClick={onClick}>
       Home
     </Button>
     <Button as={Link} to="/dashboard" w="100%" onClick={onClick}>
-      Database
+      Shitjet Sipas Ores
     </Button>
     <Button as={Link} to="/most-sold-items-by-price" w="100%" onClick={onClick}>
-      Ranking
+      Me Te Shiturat
     </Button>
   </VStack>
 );
 
 const Sidebar = ({ isOpen, variant, onClose }) => {
   return variant === 'sidebar' ? (
-    // Fixed sidebar variant
+    // Fixed sidebar variant (desktop)
     <Box
       position="fixed"
       left={0}
+      top={0}
       p={3}
       w="240px"
-      top={0}
       h="100%"
-      bg="gray.800"        // <-- Updated background color
+      bg="gray.800"
+      color="white"
       borderRight="1px solid #ddd"
+      overflowY="auto"
     >
+      {/* Optional heading for the sidebar */}
+      <Box fontSize="xl" fontWeight="bold" mb={4}>
+        Menu
+      </Box>
       <SidebarContent onClick={onClose} />
     </Box>
   ) : (
-    // Drawer variant
+    // Drawer variant (mobile)
     <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
       <DrawerOverlay>
         <DrawerContent bg="gray.800" color="white">
-          {/* Make sure the close button is visible against dark background */}
           <DrawerCloseButton color="white" />
-          <DrawerHeader borderBottomWidth="1px">Loopple Chakra</DrawerHeader>
+          <DrawerHeader borderBottomWidth="1px">Menu</DrawerHeader>
           <DrawerBody>
             <SidebarContent onClick={onClose} />
           </DrawerBody>
