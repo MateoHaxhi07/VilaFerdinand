fd43bd51c9d72d0d5296aee6620f16e7bf109729
MateoHaxhi07 <mhaxhi62@gmail.com>
2025-02-13 08:05:43 +0100

    Setup Render DAtabase PSQL

--- restaurant-backend/server.js
+++ restaurant-backend/server.js
@@ -8,12 +8,12 @@ app.use(cors());
 app.use(express.json());
 
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
 // Endpoint for all data with dynamic filters
 app.get("/sales/all-data", async (req, res) => {
   try {
