2edf90785f4025b4c4d81a4171f1437ab635c91f
MateoHaxhi07 <mhaxhi62@gmail.com>
2025-02-20 10:13:56 +0100

    t

--- src/components/Header/Layout.js
+++ src/components/Header/Layout.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Box, Button } from '@chakra-ui/react';
-import Header from './Header';
+import Header from './components/Header/index.js'; 
 
 const Layout = ({ children }) => {
   const [showHeader, setShowHeader] = useState(true);
